77b864065b77c660fb9223244d41d0f1b0174da2
Salma-Alhassan <salmaalhakim67@gmail.com>
2024-03-17 20:32:48 +0200

    dart3

---
+++ test/dart3_upacademy_test.dart
@@ -0,0 +1,8 @@
+// import 'package:dart3_upacademy/dart3_upacademy.dart';
+// import 'package:test/test.dart';
+
+// void main() {
+//   test('calculate', () {
+//     expect(calculate(), 42);
+//   });
+// }
