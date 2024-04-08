a4cc63d4a173faeb800bc10d55570019ea3964ac
Salma-Alhassan <salmaalhakim67@gmail.com>
2024-04-08 16:01:13 +0200

    afterModification

--- lib/dart3_upacademy.dart
+++ lib/dart3_upacademy.dart
@@ -1,9 +1,13 @@
 void main(){
   //!Print a countdown from 10 to 0. Using Loops.
-  for(int i=10;i>=0;i--)print(i);
+  for(int i=10;i>=0;i--) {
+    print(i);
+  }
 
   //!Print the sequence 0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8,0.9, 1.0. Using Loops.
-  for(double i=0.0;i<=1.0;i+=0.1)print(i);
+  for(double i=0.0;i<=1.0;i+=0.1) {
+    print(i.toStringAsFixed(1));
+  }
 
   //!prints out all the elements of the list that are less than 5.
   List <int> l=[1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
@@ -13,10 +17,16 @@ void main(){
 
   //!Write a program to print this pattern 1.
   for(int i=1;i<10;i++){
-    print(' '*(9-i)+'*'*i);
+    print(' '*(9-i) + '* '*i);
   }
 
 
   //!Write a program to print this pattern 2.
-  for(int i=1;i<=5;i++)print('*'*i);
+  for(int i=1;i<=5;i++) {
+    print('*' * i);
+  }
+ 
 }
+//? how to return 2 values in a function
+
+
